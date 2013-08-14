a46256e18e383b610242690c58118e3276c72eca
james2doyle <james2doyle@gmail.com>
2013-08-14 14:41:08 -0400

    add titles to form. add javascript for ajax form creation.

--- swipe/views/admin/form.php
+++ swipe/views/admin/form.php
@@ -39,6 +39,16 @@
 					<label for="stoppropagation">Stop Propagation</label>
 					<div class="input"><?php echo form_dropdown('stoppropagation', array(0 => 'false', 1 => 'true'), $stoppropagation); ?></div>
 				</li>
+				<fieldset>
+					<legend>Slide Titles</legend>
+					<small>Blank means there will be no title</small>
+					<hr>
+					<ul id="slide-titles">
+						<?php if($this->method == 'edit'){
+							echo $image_inputs;
+						} ?>
+					</ul>
+				</fieldset>
 			</ul>
 
 		</div>
@@ -50,3 +60,31 @@
 		<?php echo form_close(); ?>
 	</div>
 </section>
+<script>
+	$(document).ready(function() {
+		var $slideTitles = $('#slide-titles');
+		// get the method for this page
+		var method = '<?php echo $this->method; ?>';
+		function getInputs(folderid) {
+			$.post(BASE_URL + 'admin/swipe/image_count', {
+				folderid: folderid,
+				csrf_hash_name: $.cookie(pyro.csrf_cookie_name)
+			}, function(res){
+				$slideTitles.html(res);
+			});
+		}
+		if (method == 'create') {
+			getInputs($('select[name="folder"]').val());
+		}
+		$('select[name="folder"]').on('change', function(e) {
+			if (method == 'edit') {
+				// confirm that they want to erase the old forms when editing
+				var check = confirm('Changing the folder will empty the title fields. \nAre you sure you want to continue?');
+				if (!check) {
+					return;
+				}
+			}
+			getInputs(this.value);
+		});
+	});
+</script>
