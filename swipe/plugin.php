b579061023b7a1ecef24a9c43336e5d87cca1abe
james2doyle <james2doyle@gmail.com>
2013-08-14 14:39:29 -0400

    add cache. minify inline script. move path detection.

--- swipe/plugin.php
+++ swipe/plugin.php
@@ -31,25 +31,29 @@ class Plugin_swipe extends Plugin
 	 */
 	function slider() {
 		$id = (int)$this->attribute('id');
-		// figure out if this is in the default or shared_addons
-		// probably could be done better but hey thats what forks are for
-		$path = 'modules/swipe/js/swipe.min.js';
-		$this->load->model('swipe/swipe_m');
-		$this->lang->load('swipe');
-		$this->load->library('files/files');
-		$swipe = (array)$this->swipe_m->get($id);
+		if (!$data = $this->pyrocache->get('swipe_cache')) {
+			$this->load->model('swipe/swipe_m');
+			$this->lang->load('swipe');
+			$this->load->library('files/files');
+			$swipe = $this->swipe_m->get($id);
+			$files = Files::folder_contents($swipe->folder);
+			$swipe->files = $files['data']['file'];
+			$this->pyrocache->write($swipe, 'swipe_cache');
+		} else {
+			$swipe = $this->pyrocache->get('swipe_cache');
+		}
+		$titles = json_decode($swipe->data);
+		for ($i=0; $i < count($swipe->files); $i++) {
+			$swipe->files[$i]->swipe_title = $titles[$i];
+		}
 		if (count($swipe) > 0) {
 			// path to swipe javascript file
-			is_file(ADDONPATH.$path) ? $swipe['source'] = BASE_URL.ADDONPATH.$path : $swipe['source'] = BASE_URL.SHARED_ADDONPATH.$path;
-			$swipe['script'] = "window.swipe{$swipe[id]} = new Swipe(document.getElementById('slider_{$swipe[id]}'), {
-				startSlide: {$swipe[startslide]},
-				speed: {$swipe[speed]},
-				auto: {$swipe[auto]},
-				continuous: {$swipe[continuous]},
-				disableScroll: {$swipe[disablescroll]},
-				stopPropagation: {$swipe[stoppropagation]}
-			});";
-			return $swipe;
+			// figure out if this is in the default or shared_addons
+			// probably could be done better but hey thats what forks are for
+			$path = 'modules/swipe/js/swipe.min.js';
+			is_file(ADDONPATH.$path) ? $swipe->source = BASE_URL.ADDONPATH.$path : $swipe->source = BASE_URL.SHARED_ADDONPATH.$path;
+			$swipe->script = "window.swipe{$swipe->id} = new Swipe(document.getElementById('slider_{$swipe->id}'), {startSlide: {$swipe->startslide},speed: {$swipe->speed},auto: {$swipe->auto},continuous: {$swipe->continuous},disableScroll: {$swipe->disablescroll},stopPropagation: {$swipe->stoppropagation}});";
+			return array((array)$swipe);
 		} else {
 			return lang('swipe:no_items');
 		}
